770deb7472438265dfeaad225f5dcd91e8a426a5
Ahsen <4291744+clielmGlype4f@users.noreply.github.com>
2020-12-15 15:58:47 -0500

    pass in arg dict

--- runway_model.py
+++ runway_model.py
@@ -5,26 +5,35 @@ from PIL import Image
 from infer import InferenceWrapper
 import argparse
 
-args_dict = {
-    # 'checkpoint': directory(default="../runs/vc2-hq_adrianb_paper_main"),
-    # 'checkpoint_dir': file(is_directory=True),
-    # 'project_dir':  directory(default="."),
-    # 'init_experiment_dir': directory(default="../runs/vc2-hq_adrianb_paper_main/"),
-    # 'init_networks':  text(default='identity_embedder, texture_generator, keypoints_embedder, inference_generator') ,
-    # 'init_which_epoch': text(default="2225"),
-    # 'num_gpus':  number(default=1, min=0, max=10),
-    # 'experiment_name': text(default="vc2-hq_adrianb_paper_enhancer"), 
-    # 'which_epoch':  text(default="1225"),  
-    # 'spn_networks': text(default="identity_embedder, texture_generator, keypoints_embedder, inference_generator, texture_enhancer"),
-    # 'enh_apply_masks': boolean(default=False),
-    'inf_apply_masks': boolean(default=False)
-    #
-}
+# args_dict = {
+#     'project_dir':  directory(default="."),
+#     'init_experiment_dir': directory(default="../runs/vc2-hq_adrianb_paper_main"),
+#     'init_networks':  text(default='identity_embedder, texture_generator, keypoints_embedder, inference_generator') ,
+#     'init_which_epoch': text(default="2225"),
+#     'num_gpus':  number(default=1, min=0, max=10),
+#     'experiment_name': text(default="vc2-hq_adrianb_paper_enhancer"), 
+#     'which_epoch':  text(default="1225"),  
+#     'spn_networks': text(default="identity_embedder, texture_generator, keypoints_embedder, inference_generator, texture_enhancer"),
+#     'enh_apply_masks': boolean(default=False),
+#     'inf_apply_masks': boolean(default=False) 
+# }
 
-@runway.setup(options=args_dict)
+args_dict = {
+    'project_dir': '.',
+    'init_experiment_dir': './runs/vc2-hq_adrianb_paper_main',
+    'init_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator',
+    'init_which_epoch': '2225',
+    'num_gpus': 1,
+    'experiment_name': 'vc2-hq_adrianb_paper_enhancer',
+    'which_epoch': '1225',
+    'spn_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator, texture_enhancer',
+    'enh_apply_masks': False,
+    'inf_apply_masks': False}
+
+#@runway.setup(options=args_dict)
 def setup(opts):
 
-    return InferenceWrapper(opts)
+    return InferenceWrapper(args_dict)
 
 # translate is the function that is called when you input a image, specify the input and output types
 @runway.command('translate', inputs={'source_imgs': runway.image, "target_imgs": runway.image}, outputs={'image': runway.image})
